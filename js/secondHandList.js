7246f262a52fdb98a9d12f26a25a1e66bcd50bd7
郭绍琦 <1315678738@qq.com>
2021-12-06 16:19:24 +0800

    二手房列表页完成

---
+++ js/secondHandList.js
@@ -0,0 +1,23 @@
+var vm = new Vue({
+    el: '#app',
+        data: { 
+            lis:[],
+        },
+        methods:{
+            getData(){
+                axios.get('http://192.168.31.211:8080/second.json',{
+                    params:{}
+                }).then(res=>{
+                    this.lis = res.data;
+                    // console.log(this.lis);
+                })
+            },
+            close(){
+                this.isShow =false
+            }
+        },
+        // 挂载前调用axios获取数据
+        beforeMount () {
+            this.getData();
+        },
+})
